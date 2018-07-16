a839a073c3c144417e03b127302dff7fb0efe27f
YKSERVER <YKSERVER@YKSERVER-PC>
2018-07-16 17:21:49 +0500

    new commit

--- src/main/java/com/mycompany/esoftdesktop/MainApp.java
+++ src/main/java/com/mycompany/esoftdesktop/MainApp.java
@@ -1,26 +1,54 @@
 package com.mycompany.esoftdesktop;
 
+import java.util.Optional;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 
-
+@SpringBootConfiguration
 public class MainApp extends Application {
+
+    Alert msg;
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
-        
+
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                msg = new Alert(Alert.AlertType.CONFIRMATION);
+                msg.setTitle("Exit");
+                msg.setContentText("Do you really want to exit?");
+                
+                Optional<ButtonType> result = msg.showAndWait();
+                if (result.get() == ButtonType.OK) {
+                    System.exit(0);
+                } else {
+
+                }
+                 System.exit(0);
+            }
+        });
     }
 
     /**
@@ -33,6 +61,8 @@ public class MainApp extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+        SpringApplication.run(MainApp.class, args);
+
     }
 
 }
